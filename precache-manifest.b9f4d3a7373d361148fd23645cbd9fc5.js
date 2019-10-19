ee37b5796c3ec39bd6007e4dd4eced65843f9014
Rafael Lopez <lpz.raf@gmail.com>
2019-10-18 22:56:08 -0400

    Updates

---
+++ precache-manifest.b9f4d3a7373d361148fd23645cbd9fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7a485c4763334fdde9a44ac320efe8",
+    "url": "/react-youtube-search-app/index.html"
+  },
+  {
+    "revision": "a3fe2aff2e79597b289f",
+    "url": "/react-youtube-search-app/static/css/main.96c11df6.chunk.css"
+  },
+  {
+    "revision": "5a9c3b319892acefc84f",
+    "url": "/react-youtube-search-app/static/js/2.1893a085.chunk.js"
+  },
+  {
+    "revision": "a3fe2aff2e79597b289f",
+    "url": "/react-youtube-search-app/static/js/main.30ec9cd6.chunk.js"
+  },
+  {
+    "revision": "682cb96a420e76cd0d95",
+    "url": "/react-youtube-search-app/static/js/runtime-main.cb85638b.js"
+  }
+]);
